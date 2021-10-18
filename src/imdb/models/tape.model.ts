aeffeaf4c594f2978832d96894cfb778fdeb752e
Reynier de la Rosa <rdelarosa@ovebanco.com>
2021-10-18 13:26:18 +0200

    clean model

--- src/imdb/models/tape.model.ts
+++ src/imdb/models/tape.model.ts
@@ -1,10 +1,7 @@
-import { Field, ID, ObjectType } from "@nestjs/graphql";
+import { Field, ObjectType } from "@nestjs/graphql";
 
 @ObjectType({ description: "tape" })
 export class Tape {
-  @Field(() => ID)
-  tapeId: string;
-
   @Field()
   originalTitle: string;
 }
