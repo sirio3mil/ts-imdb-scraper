9800f20d331dc86424e5c25fca18a6f495a367cc
Reynier de la Rosa <rdelarosa@ovebanco.com>
2021-11-04 15:56:35 +0100

    refactor unit test

--- test/imdb/services/tape.service.spec.ts
+++ test/imdb/services/tape.service.spec.ts
@@ -20,53 +20,33 @@ describe('TapeService', () => {
 
   describe('getTape', () => {
 
-    it('should match original title', async () => {
-      expect(tape.originalTitle).toBe('The Matrix');
-    });
-
     it('should match ranking', async () => {
       expect(tape.ranking.calculatedScore).toBeCloseTo(8.7, 1);
       expect(tape.ranking.votes).toBeGreaterThanOrEqual(1800000);
       expect(tape.ranking.score).toBe(tape.ranking.calculatedScore * tape.ranking.votes);
     });
 
-    it('should match imdb', async () => {
-      expect(tape.ID).toBe(tapeID);
-      expect(tape.url).toBe('https://www.imdb.com/title/tt0133093/');
+    it.each([
+      ['budget', 63000000],
+      ['colors', ['Color']],
+      ['languages', ['English']],
+      ['countries', ['United States', 'Australia']],
+      ['genres', ['Action', 'Sci-Fi']],
+      ['duration', 136],
+      ['year', 1999],
+      ['ID', tapeID],
+      ['url', 'https://www.imdb.com/title/tt0133093/'],
+      ['originalTitle', 'The Matrix'],
+    ])('should match %s', async (prop, expected) => {
+      expect(tape[prop]).toEqual(expected);
     });
 
-    it('should match year', async () => {
-      expect(tape.year).toBe(1999);
-    });
-
-    it('should match duration', async () => {
-      expect(tape.duration).toBe(136);
-    });
-
-    it('should match genres', async () => {
-      expect(tape.genres).toEqual(['Action', 'Sci-Fi']);
-    });
-
-    it('should match countries', async () => {
-      expect(tape.countries).toEqual(['United States', 'Australia']);
-    });
-
-    it('should match languages', async () => {
-      expect(tape.languages).toEqual(['English']);
-    });
-
-    it('should match colors', async () => {
-      expect(tape.colors).toEqual(['Color']);
-    });
-
-    it('should match budget', async () => {
-      expect(tape.budget).toEqual(['Color']);
-    });
-
-    it('should have empty values', async () => {
-      expect(tape.credits).toEqual([]);
-      expect(tape.premieres).toEqual([]);
-      expect(tape.titles).toEqual([]);
+    it.each([
+      ['credits'],
+      ['premieres'],
+      ['titles']
+    ])('should have empty %s', async (prop) => {
+      expect(tape[prop]).toEqual([]);
     });
 
   });
