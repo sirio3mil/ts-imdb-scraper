b5a9bbdc634bdfdd51befcdee40c0b280625acfb
Reynier de la Rosa <rdelarosa@evobanco.com>
2022-12-02 22:13:57 +0100

    fix error 403

--- src/imdb/providers/abstract.provider.ts
+++ src/imdb/providers/abstract.provider.ts
@@ -5,6 +5,12 @@ import { map } from "rxjs/operators";
 
 @Injectable()
 export abstract class AbstractProvider {
+  options = {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1'
+    }
+  }
+
   constructor(protected httpService: HttpService) {}
 
   abstract get(url: URL): Promise<string>;
@@ -12,7 +18,7 @@ export abstract class AbstractProvider {
   protected async getContent(url: URL): Promise<string> {
     return lastValueFrom(
       this.httpService
-        .get(url.toString())
+        .get(url.toString(), this.options)
         .pipe(map((response) => response.data))
     );
   }
